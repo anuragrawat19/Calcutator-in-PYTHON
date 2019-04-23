7a4b020188535dca544a2331d843125238f2d529
Anurag Rawat <41102131+anuragrawat19@users.noreply.github.com>
2019-04-23 11:55:54 +0530

    Add files via upload
    simple calculator in python which does some basic arithmetic operation

---
+++ calculator/cal.py
@@ -0,0 +1,5 @@
+p=int(input("enter the principle amount"))
+r=int(input("enter the rate of intrest on principle amount"))
+t=int(input("enter the time period for simple interest"))
+simpleinterest=(p*r*t)/100
+print "simple interest is"+"-"+str(simpleinterest)
